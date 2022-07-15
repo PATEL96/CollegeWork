5adf3db68e16cee3d4315fdb6a490a19f27d598a
Raj D. Patel <patel96@Rajs-MacBook-Pro.local>
2022-07-15 23:20:27 +0530

    Updates

--- Main.java
+++ Main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        int amount;
+        int amount, find;
+
+        //Asking for Total No. of Students in the List
 
         System.out.print("Enter No. of Students: ");
         amount = read.nextInt();
         Student List[] = new Student[amount];
+
+        //Asking for Total No. of Students in the List
+
+        //Creating List of Students & getting User data for List
         
         for(int i = 0;i<amount;i++){
             for(int j = 0;j<1;j++){
@@ -16,19 +22,33 @@ class Main{
             for(int k = 0;k<1;k++){
                 String Name, Address;
                 int Enrollment;
+                System.out.println();
                 System.out.print("Name of Student "+ (i+1) +": ");
                 Name = read.next();
                 System.out.print("Address of Student "+ (i+1) +": ");
                 Address = read.next();
                 System.out.print("Enrollment of Student "+ (i+1) +": ");
                 Enrollment = read.nextInt();
+                System.out.println();
                 List[i].getDetails(i, Name, Address, Enrollment);
             }
         }
+
+        //Creating List of Students & getting User data for List
+
+        //Searching Through the List
+
+        do{
+            System.out.print("Which Student You want to Find: ");
+            find = read.nextInt();
+            System.out.println();
+            List[find-1].displayDetails(find-1);
+        }
+        while(find<amount);
+
+        //Searching Through the List
+
         
-        System.out.print("Which Student You want to Find: ");
-        int find = read.nextInt();
-        List[find-1].displayDetails(find-1);
         // Bank Patel = new Bank();
         // int Option;
         // Scanner read = new Scanner(System.in);
