e1587edac19b528d00b100aca1149ebe5bc2b440
Raj D. Patel <patel96@Rajs-MacBook-Pro.local>
2022-07-23 20:25:12 +0530

    OOPJ Practical 6

---
+++ Descending.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Descending{
+    public static void main(String []args){
+        Scanner read = new Scanner(System.in);
+        int n;
+        float temp = 0;
+
+        // Taking Marks as User Input
+        System.out.print("Length Of Student List: ");
+        n = read.nextInt();
+        float Marks[] = new float[n];
+
+        for(int i = 0;i<n;i++){
+            System.out.println();
+            System.out.print("Marks of Student "+(i+1)+" : ");
+            Marks[i] = read.nextFloat();
+            System.out.println();
+        }
+        // Taking Marks as User Input
+
+        // Interchanging The Marks according to the Requirement
+        for(int i = 0; i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if(Marks[j]>Marks[i]){
+                    temp = Marks[i];
+                    Marks[i] = Marks[j];
+                    Marks[j] = temp;
+                }
+            }
+        }
+        // Interchanging The Marks according to the Requirement
+
+        // Showing Output of the Descending Order
+        for(int i = 0;i<n;i++){
+            System.out.println((i+1) + " ==> " + Marks[i]);
+        }
+        // Showing Output of the Descending Order
+    }
+}
