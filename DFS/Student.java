9eab90a21fa1591fedc5ed8453be22e7cd531ace
PATEL96 <patel.raj.official.96@gmail.com>
2022-09-15 20:03:15 +0530

    feat: Practical 1 Done According to the Question

--- DFS/Student.java
+++ DFS/Student.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 class Student{
     public String Name;
     public String Address;
-    public int Enrollment;
+    public int Enrollment_No;
     Scanner read = new Scanner(System.in);
 
-    public void getDetails(int n, String Name, String Address, int Enrollment){
+    public void getDetails(int n, String Name, String Address, int Enrollment_No){
         this.Name = Name;
         this.Address = Address;
-        this.Enrollment = Enrollment;
+        this.Enrollment_No = Enrollment_No;
     }
     public void editDetails(int n, String Name, String Address){
         this.Name = Name;
@@ -18,6 +18,6 @@ class Student{
     public void displayDetails(int n){
         System.out.println("Name: " + this.Name);
         System.out.println("Address: " + this.Address);
-        System.out.println("Enrollment: " + this.Enrollment);
+        System.out.println("Enrollment_No: " + this.Enrollment_No);
     }
 }
