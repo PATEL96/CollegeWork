e2ebb9f15deb8bd5c9fd7919afa1dc7874fde5a0
PATEL96 <patel.raj.official.96@gmail.com>
2023-10-24 15:30:03 +0530

    feat: minor Updates.

--- SEM5/DAA/DAAPractical4.py
+++ SEM5/DAA/DAAPractical4.py
@@ -1,36 +1,40 @@
 import time
 
 def quick_sort(arr):
-	if len(arr) <=1:
-		return arr
-	
-	pivot = arr[len(arr) // 2]
-	left = [x for x in arr if x < pivot]
-	middle = [x for x in arr if x == pivot]
-	right = [x for x in arr if x > pivot]
-	
-	
-	return quick_sort(left) + middle + quick_sort(right)
-	
-
-data = []
-
-
-print("1 ==> Average Case")
-print("2 ==> Worst Case")
-opt = int(input('select Option:'))
-
-if(opt == 1):
-    st = time.time()
-    for i in range(100000):
-        data.append(i)
-    et = time.time()
-elif(opt == 2):
-    st = time.time()
-    for i in reversed(range(100000)):
-        data.append(i)
     
-    et = time.time()
+    print(arr)
+    
+    if len(arr) <=1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    print(left, middle, right)
+    
+    return quick_sort(left) + middle + quick_sort(right)
+	
+
+data = [10,14,16,22,28,34,2,10]
 
 quick_sort(data)
-print(et-st)
+# print("1 ==> Average Case")
+# print("2 ==> Worst Case")
+# opt = int(input('select Option:'))
+
+# if(opt == 1):
+#     st = time.time()
+#     for i in range(100000):
+#         data.append(i)
+#     et = time.time()
+# elif(opt == 2):
+#     st = time.time()
+#     for i in reversed(range(100000)):
+#         data.append(i)
+    
+#     et = time.time()
+
+# quick_sort(data)
+# print(et-st)
