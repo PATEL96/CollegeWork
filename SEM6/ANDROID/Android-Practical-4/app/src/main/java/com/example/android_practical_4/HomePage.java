22879bb241d11bcbd4d9d36d1f3ae0178848dcb4
PATEL96 <patel.raj.official.96@gmail.com>
2024-03-07 23:44:19 +0530

    ANDROID: Added Practical 3,4

---
+++ SEM6/ANDROID/Android-Practical-4/app/src/main/java/com/example/android_practical_4/HomePage.java
@@ -0,0 +1,31 @@
+package com.example.android_practical_4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import org.w3c.dom.Text;
+
+public class HomePage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+        Button b1 = findViewById(R.id.button);
+        Button b2 = findViewById(R.id.button2);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(HomePage.this, R.color.coal));
+
+        TextView userName = findViewById(R.id.userNameText);
+
+        Intent get1 = getIntent();
+        String Name = get1.getStringExtra("userName");
+
+        userName.setText(Name);
+    }
+}
---
+++ SEM6/ANDROID/Android-Practical-4/app/src/main/java/com/example/android_practical_4/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.android_practical_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DataHandler dh;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.coal));
+
+        Button login = findViewById(R.id.loginButton);
+        TextView sigup = findViewById(R.id.signUp);
+        login.setText("Login");
+
+        EditText userName = findViewById(R.id.userName);
+        EditText userPassword = findViewById(R.id.userPassword);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Name, Password;
+
+                Name = userName.getText().toString();
+                Password = userPassword.getText().toString();
+
+                dh = new DataHandler(getApplicationContext());
+                Cursor res = dh.Login(Name, Password);
+
+                if(res.moveToFirst()){
+                    String test = res.getString(0);
+                    Intent goHome = new Intent(MainActivity.this, HomePage.class);
+                    goHome.putExtra("userName", test);
+                    startActivity(goHome);
+                    Toast.makeText(MainActivity.this, "Welcome " + test, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "inValid UserName/Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        sigup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goSigup = new Intent(MainActivity.this, SignUpPage.class);
+                startActivity(goSigup);
+            }
+        });
+    }
+}
